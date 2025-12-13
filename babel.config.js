c662cd9c275a7bf0799b0d5986fd6407c12f37e0
zaheer <zaheerahmad92001@gmail.com>
2025-12-13 17:13:24 +0500

    reanimated issue fixed

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-      'react-native-worklets/plugin',
-    ],
+    'react-native-reanimated/plugin', // Must be last plugin - handles worklets internally
+  ],
 };
